bfeb8bb17f5f6dc77eb3af1d33d3f892dd060b7f
aremon86851 <aremon86851@gmail.com>
2023-02-04 19:46:49 +0600

    done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>this is footer</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Everything done</h1>
+      <h1>this is footer</h1>
+    </div>
+  );
 };
 
 export default Footer;
