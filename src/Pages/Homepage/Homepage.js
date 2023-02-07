4b85d04e65b24412324fb245282ee01efa50e0a9
FreelancerShijan <freelancershijan@gmail.com>
2023-02-08 03:17:58 +0600

    banner added

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HeroSection from "../../component/HeroSection";
+
+const Homepage = () => {
+    return <>
+        <HeroSection></HeroSection>
+    </>
+};
+
+export default Homepage;
