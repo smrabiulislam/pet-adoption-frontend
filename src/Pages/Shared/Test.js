68c892e9a3d91908b71c3d905f895de38b89f6d2
FreelancerShijan <freelancershijan@gmail.com>
2023-02-07 19:55:32 +0600

    shijan

---
+++ src/Pages/Shared/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div className=''>
+            <h1>this is header</h1>
+        </div>
+    );
+};
+
+export default Test;
