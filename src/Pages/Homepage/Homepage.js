c1a14216fbc6e4f7df277cfa5abd0bc04234bf97
aremon86851 <aremon86851@gmail.com>
2023-02-08 16:36:19 +0600

    converted class component

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -3,12 +3,15 @@ import CardSection from "../../component/CardSection";
 import ContactSection from "../../component/ContactSection";
 import HeroSection from "../../component/HeroSection";
 
-const Homepage = () => {
-    return <>
+class Homepage extends React.Component {
+  render() {
+    return (
+      <>
         <HeroSection></HeroSection>
         <CardSection></CardSection>
-
-    </>
-};
+      </>
+    );
+  }
+}
 
 export default Homepage;
