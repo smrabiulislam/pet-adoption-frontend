ecbeb37328695c4da4eb86292a27e8a26102f284
aremon86851 <aremon86851@gmail.com>
2023-02-08 16:28:16 +0600

    conver to class component

--- src/component/Shared/Footer/Footer.js
+++ src/component/Shared/Footer/Footer.js
@@ -2,31 +2,33 @@ import React from "react";
 import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
 import TopFooter from "./TopFooter";
 
-const Footer = () => {
-  return (
-    <>
-      <TopFooter></TopFooter>
-      <Navbar bg="light">
-        <Container>
-          <Row>
-            <Col className="text-center">
-              <Nav>
-                <Nav.Link href="#">About</Nav.Link>
-                <Nav.Link href="#">Contact us</Nav.Link>
-                <Nav.Link href="#">Terms</Nav.Link>
-                <Nav.Link href="#">Privacy</Nav.Link>
-              </Nav>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center">
-              &copy; {new Date().getFullYear()} Pet Adoption
-            </Col>
-          </Row>
-        </Container>
-      </Navbar>
-    </>
-  );
-};
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <TopFooter></TopFooter>
+        <Navbar bg="light">
+          <Container>
+            <Row>
+              <Col className="text-center">
+                <Nav>
+                  <Nav.Link href="#">About</Nav.Link>
+                  <Nav.Link href="#">Contact us</Nav.Link>
+                  <Nav.Link href="#">Terms</Nav.Link>
+                  <Nav.Link href="#">Privacy</Nav.Link>
+                </Nav>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                &copy; {new Date().getFullYear()} Pet Adoption
+              </Col>
+            </Row>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+}
 
 export default Footer;
