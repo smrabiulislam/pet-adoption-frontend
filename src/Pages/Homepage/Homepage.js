751adc59c1c87cd9f60f8630ffc65cb044978deb
FreelancerShijan <freelancershijan@gmail.com>
2023-02-08 17:18:33 +0600

    something

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AboutUsSection from "../../component/AboutUsSection";
 import CardSection from "../../component/CardSection";
-import ContactSection from "../../component/ContactSection";
+
 import HeroSection from "../../component/HeroSection";
 import PlanningAdapSection from "../../component/PlanningAdapSection";
 
