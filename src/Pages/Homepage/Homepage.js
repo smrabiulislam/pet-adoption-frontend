ff4bf961212de7dc879b7d421a78a047e8be8fb9
FreelancerShijan <freelancershijan@gmail.com>
2023-02-08 17:11:19 +0600

    2 section added

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,13 +1,16 @@
 import React from "react";
+import AboutUsSection from "../../component/AboutUsSection";
 import CardSection from "../../component/CardSection";
 import ContactSection from "../../component/ContactSection";
 import HeroSection from "../../component/HeroSection";
+import PlanningAdapSection from "../../component/PlanningAdapSection";
 
 const Homepage = () => {
     return <>
         <HeroSection></HeroSection>
         <CardSection></CardSection>
-
+        <AboutUsSection></AboutUsSection>
+        <PlanningAdapSection></PlanningAdapSection>
     </>
 };
 
