c6f9a3b512190d671cbda2d7f74ad8ce2d9e22bd
aremon86851 <aremon86851@gmail.com>
2023-02-08 16:30:38 +0600

    class componet

--- src/component/Shared/Footer/TopFooter.js
+++ src/component/Shared/Footer/TopFooter.js
@@ -1,71 +1,73 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaInstagramSquare } from "react-icons/fa";
 
-const TopFooter = () => {
-  return (
-    <footer class="bg-light py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3">
-            <h5>About Us</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              auctor, magna euismod bibendum consequat, velit velit bibendum
-              justo, vel laoreet nulla lacus ac magna.
-            </p>
-          </div>
-          <div class="col-md-3">
-            <h5>Customer Service</h5>
-            <ul class="list-unstyled">
-              <li>
-                <a href="#">Contact Us</a>
-              </li>
-              <li>
-                <a href="#">FAQ</a>
-              </li>
-              <li>
-                <a href="#">Shipping & Returns</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3">
-            <h5>My Account</h5>
-            <ul class="list-unstyled">
-              <li>
-                <a href="#">Sign In</a>
-              </li>
-              <li>
-                <a href="#">My Orders</a>
-              </li>
-              <li>
-                <a href="#">Wish List</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3">
-            <h5>Follow Us</h5>
-            <ul class="list-inline">
-              <li class="list-inline-item">
-                <a href="#">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#">
-                  <FaInstagramSquare />
-                </a>
-              </li>
-            </ul>
+class TopFooter extends React.Component {
+  render() {
+    return (
+      <footer class="bg-light py-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <h5>About Us</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                auctor, magna euismod bibendum consequat, velit velit bibendum
+                justo, vel laoreet nulla lacus ac magna.
+              </p>
+            </div>
+            <div class="col-md-3">
+              <h5>Customer Service</h5>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">Shipping & Returns</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <h5>My Account</h5>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">Sign In</a>
+                </li>
+                <li>
+                  <a href="#">My Orders</a>
+                </li>
+                <li>
+                  <a href="#">Wish List</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <h5>Follow Us</h5>
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#">
+                    <FaInstagramSquare />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-  );
-};
+      </footer>
+    );
+  }
+}
 
 export default TopFooter;
--- src/component/Shared/Header/TopHeader.js
+++ src/component/Shared/Header/TopHeader.js
@@ -8,41 +8,43 @@ import {
   FaEnvelope,
 } from "react-icons/fa";
 
-const TopHeader = () => {
-  return (
-    <Navbar bg="primary" variant="dark">
-      <Container>
-        <div className="d-flex justify-content-between align-items-center w-100">
-          <div className="text-light">
-            Welcome, <span className="text-light">Default Name</span>
+class TopHeader extends React.Component {
+  render() {
+    return (
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <div className="d-flex justify-content-between align-items-center w-100">
+            <div className="text-light">
+              Welcome, <span className="text-light">Default Name</span>
+            </div>
+            <div className="d-flex justify-content-center align-items-center gap-2">
+              <div>
+                <span className="text-light">
+                  <FaEnvelope className="me-2" />
+                  admin@petadoption.com
+                </span>
+              </div>
+              <div>
+                <a href="" className="text-light ">
+                  <FaFacebook />
+                </a>
+              </div>
+              <div>
+                <a href="" className="text-light ">
+                  <FaTwitter />
+                </a>
+              </div>
+              <div>
+                <a href="" className="text-light ">
+                  <FaInstagramSquare />
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="d-flex justify-content-center align-items-center gap-2">
-            <div>
-              <span className="text-light">
-                <FaEnvelope className="me-2" />
-                admin@petadoption.com
-              </span>
-            </div>
-            <div>
-              <a href="" className="text-light ">
-                <FaFacebook />
-              </a>
-            </div>
-            <div>
-              <a href="" className="text-light ">
-                <FaTwitter />
-              </a>
-            </div>
-            <div>
-              <a href="" className="text-light ">
-                <FaInstagramSquare />
-              </a>
-            </div>
-          </div>
-        </div>
-      </Container>
-    </Navbar>
-  );
-};
+        </Container>
+      </Navbar>
+    );
+  }
+}
 
 export default TopHeader;
